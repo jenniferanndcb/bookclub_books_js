5ad45251a32a8cd4d29f0bcdc6da611d50a0ad31
Jen <jenniferanndcb@gmail.com>
2020-01-18 16:46:47 +0000

    render bookclubs

--- bookclub_books_frontend/src/index.js
+++ bookclub_books_frontend/src/index.js
@@ -3,14 +3,26 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function fetchBookClubs() {
-  fetch("http://localhost:3000/bookclubs").then(function(res) {
-    return res.json();
-  })
-  .then(function(data) {
-    return data.map(function(bookclub){
-      return renderBookClub(bookclub);
+  fetch("http://localhost:3000/bookclubs")
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      return data.map(function(bookclub) {
+        return renderBookClub(bookclub);
+      });
     });
-  });
 }
 
+function renderBookClub(bookclub) {
+  const bookclubList = document.getElementById("bookclubs-list");
+  const card = document.createElement("div");
+  card.className = "card";
 
+  bookclubList.append(card);
+
+  const h2 = document.createElement("h2");
+  h2.innerText = bookclub.name;
+
+  card.append(h2);
+}
