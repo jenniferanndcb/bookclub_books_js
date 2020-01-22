7d3fa43cc1c062a1bc19c75c1221e8538654af86
Jen <jenniferanndcb@gmail.com>
2020-01-22 21:43:07 +0000

    cr details in a div

--- bookclub_books_frontend/src/components/bookclub.js
+++ bookclub_books_frontend/src/components/bookclub.js
@@ -13,6 +13,15 @@ class BookClub {
 
   findCurrentlyReading() {
     let book = this.books.find(book => book.currently_reading);
-    return book ? book.title : false;
+    return book ? this.renderBook(book) : false;
+  }
+
+  renderBook(book) {
+    // console.log(book);
+    return `<div class="bookdetails">
+    <img src= ${book.image}/>
+    <h3>${book.title}</h3>
+    <h4>by ${book.author}</h4>
+    </div>`;
   }
 }
